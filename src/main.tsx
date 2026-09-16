11bbacdc7339d34118781acec65bea5af520ba42
Sumit Mehra <sumitmehraguru@gmail.com>
2026-09-16 16:45:39 +0530

    Add live public share routing and share manager

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './TournamentApp';
+import { mountPublicShare } from './PublicShareApp';
+import { initShareManager } from './share-manager';
 import './styles.css';
 import './score-modern.css';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const root=document.getElementById('root')!;
+const isPublicShare=new URLSearchParams(window.location.search).has('share');
+
+if(isPublicShare){
+  mountPublicShare();
+}else{
+  ReactDOM.createRoot(root).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+  initShareManager();
+}
