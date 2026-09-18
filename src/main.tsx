57828eb8c07e9684bccebff15ce55facb09fa99a
Sumit Mehra <sumitmehraguru@gmail.com>
2026-09-18 13:48:43 +0530

    Handle Supabase password recovery route

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './TournamentApp';
+import ResetPasswordPage from './ResetPasswordPage';
 import { mountPublicShare } from './PublicShareApp';
 import { initShareManager } from './share-manager';
 import './styles.css';
@@ -9,7 +10,9 @@ import { TermsPage, PrivacyPage } from './LegalPages';
 
 const root=document.getElementById('root')!;
 const path=window.location.pathname;
-const isPublicShare=new URLSearchParams(window.location.search).has('share');
+const params=new URLSearchParams(window.location.search);
+const isPublicShare=params.has('share');
+const isRecovery=window.location.hash.includes('type=recovery')||params.get('type')==='recovery';
 const isTerms=path === '/terms' || path === '/terms/';
 const isPrivacy=path === '/privacy' || path === '/privacy/';
 
@@ -17,6 +20,8 @@ if(isTerms){
   ReactDOM.createRoot(root).render(<TermsPage />);
 }else if(isPrivacy){
   ReactDOM.createRoot(root).render(<PrivacyPage />);
+}else if(isRecovery){
+  ReactDOM.createRoot(root).render(<React.StrictMode><ResetPasswordPage/></React.StrictMode>);
 }else if(isPublicShare){
   mountPublicShare();
 }else{
